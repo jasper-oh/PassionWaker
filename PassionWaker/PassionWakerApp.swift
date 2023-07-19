b7d5f79b89251334d80df1b41be14e0b4fc23215
Jasper Oh <ohyj0906@gmail.com>
2023-07-19 09:43:29 +0900

    Initial Commit

---
+++ PassionWaker/PassionWakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassionWakerApp.swift
+//  PassionWaker
+//
+//  Created by Jasper Oh on 2023-07-19.
+//
+
+import SwiftUI
+
+@main
+struct PassionWakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
